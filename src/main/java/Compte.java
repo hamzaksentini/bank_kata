ae8b07fae99e482fc629dfa1b704fb1cf8639795
sana.azizi <sanaazizi@2020>
2022-08-11 20:42:10 +0200

    Implement deposit method

--- src/main/java/Compte.java
+++ src/main/java/Compte.java
@@ -6,40 +6,22 @@ import java.time.LocalDateTime;
 
 public class Compte {
 
-    private int balance = 0;
+    private double balance = 0;
     private int amount ;
 
     public Compte(){
         this.balance = 0;
     }
 
-    public int returnBalance(){
+    public double getBalance(){
         return (balance);
     }
 
-    public int deposit(){
-        Scanner reader = new Scanner(System.in);
-        System.out.println("Type the amount of money to add");
-        amount = reader.nextInt();
+    public double deposit(double amount){
         balance = balance + amount ;
-        reader.close();
         return balance ;
     }
 
-    public int withdraw(){
-        Scanner reader = new Scanner(System.in);
-        System.out.println("Type the amount of money to add");
-        amount = reader.nextInt();
-        balance = balance - amount ;
-        reader.close();
-        return balance ;
-    }
-    public static boolean isToday(Date date) {
-        Date now = new Date();
-        boolean result = true;
-        result &= date.getDate() == now.getDate();
-        return result;
-    }
 
 
 
--- src/test/java/CompteTest.java
+++ src/test/java/CompteTest.java
@@ -1,8 +1,5 @@
-import junit.framework.TestCase;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
-
-import static java.util.function.Predicate.isEqual;
 
 
 public class CompteTest{
@@ -12,10 +9,20 @@ public class CompteTest{
         //Given
         Compte compte = new Compte();
         //When
-        int balance = compte.returnBalance();
+        double balance = compte.getBalance();
         //Then
         Assertions.assertThat(balance).isEqualTo(0);
     }
 
+    @Test
+    public void should_deposit_amount(){
+        //Given
+        Compte compte = new Compte();
+        //When
+        compte.deposit(50d);
+        //Then
+        double balance = compte.getBalance();
+        Assertions.assertThat(balance).isEqualTo(50);
+    }
 
 }
